7c299960842f00cb6bdc66d27da6ae32ca94957b
MojicaMhace <168732203+MojicaMhace@users.noreply.github.com>
2024-10-18 23:03:13 +0800

    Add files via upload

---
+++ Models/Credentials.cs
@@ -0,0 +1,9 @@
+﻿namespace GradeHub.Models
+{
+    public class Credentials
+    {
+        public string StudentName { get; set; }
+        public string CourseSection { get; set; }
+        public double Average { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GradeHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
